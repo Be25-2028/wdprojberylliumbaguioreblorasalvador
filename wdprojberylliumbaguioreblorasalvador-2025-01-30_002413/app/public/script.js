11589cc01abf7878206caa43f9b46c203cd193bd
Be25-2028 <b2028mihreblora@pshs.edu.ph>
2025-01-30 08:36:35 +0800

    Add files via upload

---
+++ wdprojberylliumbaguioreblorasalvador-2025-01-30_002413/app/public/script.js
@@ -0,0 +1,7 @@
+// When the respective buttons are interacted, it calls a function respective to what is typed
+
+function playhangman(){
+        playhangmanimg.src="https://cdn.glitch.global/19eab36a-e663-4934-90c7-7d1aa5c8acf9/placeholder_gamescreen.png?v=1734964194864";
+      }
+
+// the function then changes the image with their respective ids
